28e13844849c6f2216d5ffacf230cb18eedd507e
Manuel Reil <manuel.reil@alyne.com>
2019-10-09 17:14:59 +0200

    Removes typescript version, adds linting, adds comma replacement, uses yarn, reshapes packag.json

--- index.js
+++ index.js
@@ -28,19 +28,20 @@
  * @return {String}         Sanitized filename
  */
 
-var truncate = require("truncate-utf8-bytes");
+var truncate = require('truncate-utf8-bytes');
 
-var illegalRe = /[\/\?<>\\:\*\|"]/g;
-var controlRe = /[\x00-\x1f\x80-\x9f]/g;
+var illegalRe = /[/?<>\\:*|"]/g;
+var controlRe = /[\x00-\x1f\x80-\x9f]/g; //eslint-disable-line
 var reservedRe = /^\.+$/;
-var windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i;
-var windowsTrailingRe = /[\. ]+$/;
+var windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i; //eslint-disable-line
+var windowsTrailingRe = /[. ]+$/;
 
 function sanitize(input, replacement) {
   if (typeof input !== 'string') {
     throw new Error('Input must be string');
   }
   var sanitized = input
+    .replace(/,/, replacement)
     .replace(illegalRe, replacement)
     .replace(controlRe, replacement)
     .replace(reservedRe, replacement)
@@ -49,7 +50,7 @@ function sanitize(input, replacement) {
   return truncate(sanitized, 255);
 }
 
-module.exports = function (input, options) {
+module.exports = function(input, options) {
   var replacement = (options && options.replacement) || '';
   var output = sanitize(input, replacement);
   if (replacement === '') {
